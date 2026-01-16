e369e90fa1f2d280740a17e91f075ffee30270f1
seok6772 <jeasuk6772@naver.com>
2026-01-16 11:18:01 +0900

    라우터 기반 성적처리 앱 성적 등록 구현

--- routers/sungjuk.py
+++ routers/sungjuk.py
@@ -8,16 +8,45 @@ router = APIRouter(prefix="/sungjuk", tags=["sungjuk"])
 
 @router.get("/list", response_class=HTMLResponse)
 async def sungjuk_list(request: Request):
-    pass
+    async with aiosqlite.connect(SungJukDB_NAME) as db:
+        results = await db.execute_fetchall("""
+        SELECT sjno, name, kor, eng, mat
+        FROM sungjuk ORDER BY sjno DESC""")
+
+    # 조회된 결과집합을 html에서 처리하기 편하게 JSON형식으로 변환
+    sungjuks = []
+    for rs in results:
+        sungjuk = {
+            "sjno": rs[0],
+            "name": rs[1],
+            "kor": rs[2],
+            "eng": rs[3],
+            "mat": rs[4]
+        }
+        sungjuks.append(sungjuk)
+
+    return templates.TemplateResponse("sungjuk/sungjuk_list.html", {
+        "request": request,
+        "sungjuks": sungjuks
+    })
 
 @router.get("/new", response_class=HTMLResponse)
 async def sungjuk_newform(request: Request):
-    pass
+    return templates.TemplateResponse("sungjuk/sungjuk_new.html", {"request": request})
 
 @router.post("/new", response_class=HTMLResponse)
 async def sungjuk_new(request: Request, name: str = Form(...),
         kor: int = Form(...), eng: int = Form(...), mat: int = Form(...)):
-    pass
+    # 성적 처리 (총점/평균/학점 계산)
+    tot, avg, grd =compute_sungjuk(kor, eng, mat)
+
+    async with aiosqlite.connect(SungJukDB_NAME) as db:
+        await db.execute(
+            "insert into sungjuk (name,kor,eng,mat,tot,avg,grd) values (?, ?, ?, ?, ?, ?, ?)",
+            (name, kor, eng, mat, tot, avg, grd))
+        await db.commit()
+
+    return RedirectResponse(url="/sungjuk/list", status_code=303)
 
 @router.get("/{sjno}", response_class=HTMLResponse)
 async def sungjuk_detail(request: Request, sjno: int):
@@ -36,3 +65,18 @@ async def sungjuk_edit(request: Request, sjno: int,
         kor: int = Form(...), eng: int = Form(...), mat: int = Form(...)):
     pass
 
+
+def compute_sungjuk(kor, eng, mat):
+    """
+    성적 데이터에 대한 총점,평균,학점 처리
+
+    :return:
+    """
+    tot = mat + eng + kor
+    avg = tot / 3
+    grd = ('A' if (avg >= 90) else
+           'B' if (avg >= 80) else
+           'C' if (avg >= 70) else
+           'D' if (avg >= 60) else 'F')
+
+    return [tot, avg, grd]
